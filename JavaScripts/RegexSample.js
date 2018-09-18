1a15a21b4127dd67c9bd379a2975f6706ef7d2fe
hongyl <hongyl@outlook.com>
2018-09-18 23:21:31 +0800

    add javascript - Regex

---
+++ JavaScripts/RegexSample.js
@@ -0,0 +1,14 @@
+// https://playcode.io/123129?tabs=console&script.js&output
+
+// https://regexr.com/
+
+var source = "AAA BBB yyy(10, 2121); CCCC DDD yyy(222, 333);";
+var reg = new RegExp(/(yyy\()(\d+),\s+(\d+)/g);
+// $1: (yyy\()   [first match]
+// $2: (\d+)     [second match]
+// $3: (\d+)     [third match]
+var ss = source.replace(reg, "xxx($2," + " $3" + ", eee");
+// ss = AAA BBB xxx(10, 2121, eee); CCCC DDD xxx(222, 333, eee);
+
+console.log(ss);
+
